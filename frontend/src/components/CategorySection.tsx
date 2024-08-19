db34b707421daaecafa8564fe5f82c9f2226064d
subh05sus <sahasubhadipfkk@gmail.com>
2024-08-19 21:10:03 +0530

    new category title

---
+++ frontend/src/components/CategorySection.tsx
@@ -0,0 +1,12 @@
+import CategoryTitles from "./CategoryTitles"
+
+
+function CategorySection() {
+  return (
+    <div>
+        <CategoryTitles/>
+    </div>
+  )
+}
+
+export default CategorySection
--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { getBanners } from "../api-client";
 import BannerCarousel from "../components/BannerCarousel";
 import ProductCarousel from "../components/ProductCarousel";
+import CategorySection from "../components/CategorySection";
 
 const HomePage: React.FC = () => {
   const [banners, setBanners] = useState<any[]>([]);
@@ -24,6 +25,7 @@ const HomePage: React.FC = () => {
     <div>
       <BannerCarousel banners={banners} />
     <ProductCarousel/>
+    <CategorySection/>
     </div>
   );
 };
