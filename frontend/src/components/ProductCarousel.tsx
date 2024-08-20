4d1d96eebba3f17f97ad2a9ecc64082f184cca41
subh05sus <sahasubhadipfkk@gmail.com>
2024-08-21 01:35:53 +0530

    everything is fine

--- frontend/src/components/ProductCarousel.tsx
+++ frontend/src/components/ProductCarousel.tsx
@@ -1,17 +1,23 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState, useEffect } from "react";
-import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
-import 'react-horizontal-scrolling-menu/dist/styles.css';
+// import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+// import 'react-horizontal-scrolling-menu/dist/styles.css';
 import { getFeaturedProducts, getFootwearProducts, getJerseys } from "../api-client";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import { PiSneaker } from "react-icons/pi";
 import { TbShirtSport } from "react-icons/tb";
 import { HeroText } from "./HeroText";
 import { Link } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
 const ProductCarousel: React.FC = () => {
   const [featuredProducts, setFeaturedProducts] = useState<any[]>([]);
   const [footwearProducts, setFootwearProducts] = useState<any[]>([]);
   const [jerseys, setJerseys] = useState<any[]>([]);
+
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -28,28 +34,48 @@ const ProductCarousel: React.FC = () => {
     };
 
     fetchProducts();
-  }, []);
- 
-  const LeftArrow = () => {
-    const { scrollPrev } = React.useContext(VisibilityContext);
+  }, []); 
+
+  
+  const LeftArrow = (props: any) => {
+    const { onClick } = props;
     return (
-      <button onClick={() => scrollPrev()} className="absolute left-0 top-1/2 transform -translate-y-1/2 text-gray-800 z-50 bg-white mx-4 p-4 border shadow shadow-slate-500  rounded-full">
+      <button
+        onClick={onClick}
+        className="absolute left-0 top-1/2 transform -translate-y-1/2 text-gray-800 z-50 bg-white mx-4 p-4 border shadow shadow-slate-500 rounded-full"
+      >
         <FaArrowLeft />
       </button>
     );
   };
 
-  const RightArrow = () => {
-    const { scrollNext } = React.useContext(VisibilityContext);
+  const RightArrow = (props: any) => {
+    const { onClick } = props;
     return (
-      <button onClick={() => scrollNext()} className="absolute right-0 top-1/2 transform -translate-y-1/2 text-gray-800 z-50 bg-white mx-4 p-4 border shadow shadow-slate-500  rounded-full">
+      <button
+        onClick={onClick}
+        className="absolute right-0 top-1/2 transform -translate-y-1/2 text-gray-800 z-50 bg-white mx-4 p-4 border shadow shadow-slate-500 rounded-full"
+      >
         <FaArrowRight />
       </button>
     );
   };
 
+
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    
+    prevArrow:<LeftArrow/>,
+    nextArrow: <RightArrow/>,
+  };
+
   const ProductCard = ({ product }: { product: any }) => (
-    <Link className="flex-shrink-0 w-52 bg-slate-100 p-4 rounded-md border hover:bg-white  hover:shadow-xl" to={`/product/${product._id}`}>
+    <Link className="  bg-slate-200 p-4 rounded-xl border hover:bg-white  hover:shadow-xl" to={`/product/${product._id}`}>
       <img src={product.imageUrl.split(',')[0]} alt={product.title} className="w-full h-32 object-cover mb-2" />
       <h3 className="font-semibold">{product.title}</h3>
       <p className="text-xs text-gray-600">{product.shortDescription}</p>
@@ -58,14 +84,21 @@ const ProductCarousel: React.FC = () => {
 
   return (
     <div className="p-4">
-      <h2 className="text-6xl portrait:text-3xl font-bold mt-8 mb-4 text-left poppins-bold">Featured <span className="text-orange-600 hover:text-slate-700 duration-300 transition-all">Products</span></h2>
-      <div className="relative">
-        <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} scrollContainerClassName="no-visible-scrollbar gap-2">
-          {featuredProducts.map((product) => (
-            <ProductCard key={product._id} product={product} />
-          ))}
-        </ScrollMenu>
+      <h2 className="text-6xl portrait:text-3xl font-bold mt-8 mb-4 text-left poppins-bold">Featured <span className="text-orange-600 hover:text-slate-200 duration-300 transition-all">Products</span></h2>
+      <div className="bg-white">
+  <Slider {...settings} className="w-full" centerPadding="30px">
+    {featuredProducts.map((product) => (
+    <Link className=" p-1" to={`/product/${product._id}`}>
+      <div className=" bg-white border-2 p-4  rounded-xl border hover:bg-white  hover:shadow-xl">
+
+    <img src={product.imageUrl.split(',')[0]} alt={product.title} className="w-full h-32 object-cover mb-2" />
+    <h3 className="font-semibold">{product.title}</h3>
+    <p className="text-xs text-gray-600">{product.shortDescription}</p>
       </div>
+  </Link>    ))}
+  </Slider>
+</div>
+
 
           <HeroText/>
 
