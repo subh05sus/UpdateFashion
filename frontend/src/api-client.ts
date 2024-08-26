83178788caf4f374978d2cfcb52bf0a34cccb5b0
subh05sus <sahasubhadipfkk@gmail.com>
2024-08-26 18:03:54 +0530

    fixed base url issue

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -2,7 +2,7 @@
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
 
 // ------------------------------------user
 
